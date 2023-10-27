17b7b43f20e0e8efc9a7ea276d9faf2e234500d5
Valeria Perdomo <valeriaperdomogarcia93@gmail.com>
2023-10-27 16:39:11 -0500

    probando commit para el repo

--- c#/Average of Four.cs
+++ c#/Average of Four.cs
@@ -4,16 +4,16 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Ingrese el primer número: ");
+        Console.Write("Por favor ingrese el primer número: ");
         if (double.TryParse(Console.ReadLine(), out double numero1))
         {
-            Console.Write("Ingrese el segundo número: ");
+            Console.Write("Por favor ingrese el segundo número: ");
             if (double.TryParse(Console.ReadLine(), out double numero2))
             {
-                Console.Write("Ingrese el tercer número: ");
+                Console.Write("Por favor ingrese el tercer número: ");
                 if (double.TryParse(Console.ReadLine(), out double numero3))
                 {
-                    Console.Write("Ingrese el cuarto número: ");
+                    Console.Write("Por favor ingrese el cuarto número: ");
                     if (double.TryParse(Console.ReadLine(), out double numero4))
                     {
                         double promedio = (numero1 + numero2 + numero3 + numero4) / 4;
